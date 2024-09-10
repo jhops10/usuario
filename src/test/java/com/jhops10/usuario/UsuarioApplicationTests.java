78d9a64a1a346fff7b4ebf99074bfc35c594ca86
João Paulo <joaowais@gmail.com>
2024-09-10 19:32:45 -0300

    commit inicial

---
+++ src/test/java/com/jhops10/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhops10.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
